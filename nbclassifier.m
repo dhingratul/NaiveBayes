cf6982399f9eba790b74b224b35c25e2f552f3b9
dhingratul <atul.dhingra@rutgers.edu>
2016-01-30 12:16:42 -0500

    Naive Bayes: MLE

--- nbclassifier.m
+++ nbclassifier.m
@@ -3,6 +3,9 @@ close all;
 clc;
 count0=0;
 count1=0;
+count=0;
+a=1;
+b=1;
 load reuters.mat;
 mat1=zeros(1,size(train,2));
 mat0=zeros(1,size(train,2));
@@ -27,8 +30,25 @@ for i=1:length(trainy)
 end
 P_0=count0/length(trainy);
 P_1=count1/length(trainy);
+mat0=mat0./sum(mat0);
+mat1=mat1./sum(mat1);
 
-
-% mat0=mat0./count0;
-% mat1=mat1./count1;
-
+%% Classifier
+for i=1:length(testy)
+    
+    a=P_0*prod(mat0.^test(i,:));
+    b=P_1*prod(mat1.^test(i,:));
+    
+    if(a>b)
+        output(i,1)=0;
+    else
+        output(i,1)=1;
+    end
+end
+%% Accuracy
+for i=1:length(output)
+    if(output(i,1)==testy(i,1))
+        count=count+1;
+    end
+end
+per=count/length(testy)*100
