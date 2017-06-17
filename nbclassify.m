c73c6f4725c3384f413a3e16b4a6cb418cf4cfe2
dhingratul <atul.dhingra@rutgers.edu>
2017-06-17 18:19:07 -0400

    Documentation

--- nbclassify.m
+++ nbclassify.m
@@ -1,29 +1,34 @@
+%% Naive Bayes Classifer
+% Input: Training data, training label, testing data, testing label,
+% parameters
+% Output: Percentage accuracy, predicted labels
+% Note: Run from test_run.m
 function[per_avg,out_label,output1]=nbclassify(train,labeltr,test,labelte,alpha)
-    %% Initializing frequency count.
-    test=full(test);
-    labelte=full(labelte);
-    train=full(train);
-    labeltr=full(labeltr);
-    F1=zeros(1,size(train,2));
-    F0=zeros(1,size(train,2));
-    count0=0;
-    count1=0; 
-    ctr1=0; ctr=0;
-    c0=0;c1=0;itr=1;
-    V=size(train,2);    %Size of vocabulary
+%% Initializing frequency count.
+test=full(test);
+labelte=full(labelte);
+train=full(train);
+labeltr=full(labeltr);
+F1=zeros(1,size(train,2));
+F0=zeros(1,size(train,2));
+count0=0;
+count1=0;
+ctr1=0; ctr=0;
+c0=0;c1=0;itr=1;
+V=size(train,2);    %Size of vocabulary
 %     for alpha=1:1:1000
-    %% Calculating fraction of documents that belonging to class 0, and class 1
+%% Calculating fraction of documents that belonging to class 0, and class 1
 for i=1:length(labeltr)
     if(labeltr(i)==0)
         count0=count0+1;
         for j=1:size(train,2)
-        F0(1,j)=F0(1,j)+train(i,j); % Average Frequency of each word in class 0
+            F0(1,j)=F0(1,j)+train(i,j); % Average Frequency of each word in class 0
         end
         F0=F0+(alpha-1);%Dirichlet Prior
     else
         count1=count1+1;
-        for j=1:size(train,2)  
-        F1(1,j)=F1(1,j)+train(i,j); % Average Frequency of each word in class 1
+        for j=1:size(train,2)
+            F1(1,j)=F1(1,j)+train(i,j); % Average Frequency of each word in class 1
         end
         F1=F1+(alpha-1);%Dirichlet Prior
     end
@@ -53,7 +58,7 @@ for i=1:length(labelte)
     end
 end
 ctr=0;
-%% Accuracy 
+%% Accuracy
 for i=1:length(out_label)
     if(out_label(i,1)==labelte(i,1))
         ctr=ctr+1;
@@ -62,10 +67,10 @@ end
 per_avg(itr,2)=ctr/length(labelte);
 per_avg(itr,1)=alpha;
 itr=itr+1;
-    count0=0;
-    count1=0; 
-    ctr=0;
-    c0=0;c1=0;
+count0=0;
+count1=0;
+ctr=0;
+c0=0;c1=0;
 
 end
 % end
